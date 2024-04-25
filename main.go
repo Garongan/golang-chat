a1b237c3b16cd73813e3c39a1186977c2a712cef
alvindo <alvindo56@gmail.com>
2024-04-25 20:10:50 +0700

    feat: maxProfit package

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ func main() {
 
 	// create database schema for basic chat messaging system with postgresql
 	initdb.InitDb()
-	// get max profit from stock prices
-	prices := []int{3, 2, 6, 5, 0, 3}
-	k := 2
 
-	maxprofit := maxprofit.MaxProfit(prices, k)
+	// get max profit from stock prices
+	stokPrices := []int{3, 2, 6, 5, 1, 3} // sample price stok list of each day
+	maxTransaction := 2 // maximum transaction allowed
+
+	maxprofit := maxprofit.MaxProfit(stokPrices, maxTransaction)
 
 	// Menghitung dan mencetak keuntungan maksimal
 	fmt.Println("Keuntungan maksimal yang dapat diperoleh:", maxprofit)
--- maxprofit/maxprofit.go
+++ maxprofit/maxprofit.go
@@ -1,37 +1,32 @@
 package maxprofit
 
-func MaxProfit(prices []int, k int) int {
-	n := len(prices)
-	// Kasus k terlalu besar, dapat melakukan transaksi sebanyak yang diinginkan
-	if k >= n/2 {
-		maxProfit := 0
-		for i := 1; i < n; i++ {
-			if prices[i] > prices[i-1] {
-				maxProfit += prices[i] - prices[i-1]
+func MaxProfit(prices []int, maxTransaction int) int {
+	profit := 0
+	if len(prices) == 1 {
+		// tidak ada keuntungan
+		return profit
+	}
+
+	// pembelian saham pertama
+	buy := prices[0]
+	for index, price := range prices {
+		transaction := 0
+		if transaction < maxTransaction && index+1 < len(prices) && buy != 0 && buy < price {
+			// penjualan => profit bertambah
+			profitTemp := price - buy
+			transaction++
+
+			if profitTemp > profit {
+				profit = profitTemp
+			}
+
+			if profit > price {
+				buy = prices[index+1]
+			} else {
+				buy = 0
 			}
 		}
-		return maxProfit
 	}
 
-	dp := make([][]int, k+1)
-	for i := range dp {
-		dp[i] = make([]int, n)
-	}
-
-	for i := 1; i <= k; i++ {
-		maxPrevDiff := -prices[0]
-		for j := 1; j < n; j++ {
-			dp[i][j] = max(dp[i][j-1], prices[j]+maxPrevDiff)
-			maxPrevDiff = max(maxPrevDiff, dp[i-1][j]-prices[j])
-		}
-	}
-
-	return dp[k][n-1]
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return profit
 }
