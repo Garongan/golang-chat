87544086f037848ac1c17b065d4fba9afc8d5580
alvindo <alvindo56@gmail.com>
2024-04-27 18:02:54 +0700

    feat: example uages create new group

--- main.go
+++ main.go
@@ -18,27 +18,49 @@ func main() {
 	defer mongoactions.Disconnect()
 
 	// create new user
-	newUser, err := mongoactions.CreateUser("John Babi", "08123456789", "password")
-	if err != nil {
-		fmt.Println(err)
-	}
+	/*
+		newUser, err := mongoactions.CreateUser("John Babi", "08123456789", "password")
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	fmt.Println("new user: ", newUser)
+		fmt.Println("new user: ", newUser)
+	*/
 
 	// create new conversations
 	// create new list of messages
-	newMessage := mongoactions.CreateMessage("sender_id", "receiver_id", "Hello, how are you?", "unread", nil)
+	/*
+		newMessage := mongoactions.CreateMessage("sender_id", "receiver_id", "Hello, how are you?", "unread", nil)
 
-	newMessages := make([]chatmodel.Message, 0)
-	newMessages = append(newMessages, newMessage)
+		newMessages := make([]chatmodel.Message, 0)
+		newMessages = append(newMessages, newMessage)
 
-	newConversation, err := mongoactions.CreateConversation("sender_id", "receiver_id", newMessages)
-	if err != nil {
-		fmt.Println(err)
-	}
+		newConversation, err := mongoactions.CreateConversation("sender_id", "receiver_id", newMessages)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	fmt.Println("new conversation: ", newConversation)
+		fmt.Println("new conversation: ", newConversation)
+	*/
 
+	// create new list of message
+	// create new group
+	/*
+		newMessage := mongoactions.CreateMessage("sender_id", "receiver_id", "Hello, how are you?", "unread", nil)
+		newMessages := make([]chatmodel.Message, 0)
+		newMessages = append(newMessages, newMessage)
+
+		newGroupMembers := make([]chatmodel.GroupMember, 0)
+		newGroupMembers = append(newGroupMembers, mongoactions.CreateGroupMember("admin_id", "admin"))
+
+		newGroup, err := mongoactions.CreateGroup("admin_id", newMessages, newGroupMembers)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("new group: ", newGroup)
+	*/
+	
 	// get max profit from stock prices
 	stokPrices := []int{3, 2, 6, 5, 1, 3} // sample price dari saham pada setiap harinya
 	maxTransaction := 2                   // transaksi maksimal yang diperbolehkan
